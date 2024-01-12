f1007d507ffad6cb54a17dac9cbdd0d1d27e72e3
PVCBia <pvcbeatriz@gmail.com>
2024-01-12 15:53:01 -0300

    ajustes no analyzer.js

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,23 +1,35 @@
 const analyzer = {
-  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+  //esta função deve retornar a contagem de palavras 
+  //encontradas no parâmetro `text` do tipo`string`.
   getWordCount: (text) => {
-    const words = text.trim().split(' ');
+    const words = text.trim().split(' '); //remove os espaços em branco e divede a string em palavras
     return words.length;
+  //remove os espaços em branco e divede a string em palavras
   },
-  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  //esta função deve retornar a contagem de caracteres 
+  //encontrados no parâmetro `text` do tipo`string`.
   getCharacterCount: (text) => {
-    return text.length;
+    return text.length; //contagem total de caracteres em uma string usando a propriedade length
   },
-  //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+  //Esta função deve retornar a contagem de caracteres 
+  //excluindo espaços e sinais de pontuação encontrados 
+  //no parâmetro `text` do tipo`string`.
   getCharacterCountExcludingSpaces: (text) => {
     const characters = text.replace(/[^a-zA-Z0-9]/g, '');
     return characters.length;
+    //número de caracteres em uma string, excluindo espaços e sinais de pontuação
+    ///[^a-zA-Z0-9]/g para substituir todos os caracteres que não
+    //são letras ou números por uma string vazia
+    //retorna o comprimento da string
   },
 
 
 
-  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  /*getNumberCount: (text) => {
+  //esta função deve retornar quantos números estão no
+  //parâmetro `text` do tipo`string`.
+  
+  getNumberCount: (text) => {
     const numbers = text.split(/\D/);
     let count = 0;
     for (let i = 0; i < numbers.length; i++) {
@@ -27,14 +39,17 @@ const analyzer = {
     }
     return count;
   },
-*/
-  getNumberCount: (text) => {
+
+  /*getNumberCount: (text) => {
     const numbers = text.match(/\d+/g) || [];
     return numbers.length;
-  },
+    //número de ocorrências de números em uma strin
+    //  (/\d+/g) para encontrar todos os dígitos na string
+    //retorna o comprimento resultante  //array?
+  }, */
 
-
-  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  //esta função deve retornar a soma de todos os números
+  //encontrados no parâmetro `text` do tipo`string`.
   getNumberSum: (text) => {
     const numbers = text.split(/\D/);
     let sum = 0;
@@ -44,17 +59,29 @@ const analyzer = {
       }
     }
     return sum;
+    
+    //Calcula a soma de todos os números em uma string
+    //usa uma expressão regular semelhante para identificar os números na 
+    //string e, em seguida, percorre o array resultante
+    //convertendo cada elemento para um número inteiro e somando - os.
   },
 
-  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  //esta função deve retornar o comprimento médio das 
+  //palavras encontradas no parâmetro `text` do tipo`string`.
   getAverageWordLength: (text) => {
     const words = text.trim().split(' ');
     let wordLength = 0;
     for (let i = 0; i < words.length; i++) {
       wordLength += words[i].length;
     }
-    const averageWordLength = wordLength / words;
+    const averageWordLength = wordLength / words.length;
     return parseFloat(averageWordLength.toFixed(2));
+
+    //calcula o comprimento médio das palavras em uma string
+    //divide a string em palavras e, em seguida, calcula a soma dos 
+    //comprimentos das palavras e a divide pelo número total de palavras
+    //para obter a média
+    //O resultado é então arredondado para duas casas decimais.
   },
 };
 
